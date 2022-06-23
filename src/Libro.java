42d31adec2596298fbc436bcc1ac5c354bdbac83
neoengelus <neodarklink@gmail.com>
2022-06-22 21:12:50 -0300

    second commit

--- src/Libro.java
+++ src/Libro.java
@@ -1,4 +1,82 @@
-
 public class Libro {
-
+	private String nombre;
+	private String autor;
+	private String descripcion;
+	private int anioPub;
+	private int cantPag;
+	
+	//constructores 
+	
+	public Libro() {
+		
+	}
+	
+	public Libro(String nom, String aut, String desc, int pub, int pag) {
+		this.nombre = nom;
+		this.autor = aut;
+		this.descripcion = desc;
+		this.anioPub = pub;
+		this.cantPag = pag;
+	}
+	
+	public Libro(Libro l) {
+		this.nombre = l.nombre;
+		this.autor = l.autor;
+		this.descripcion = l.descripcion;
+		this.anioPub = l.anioPub;
+		this.cantPag = l.cantPag;
+	}
+	
+	//metodos set y get
+	
+	public void setNombre(String n) {
+		this.nombre = n;
+	}
+	
+	public void setAutor(String a) {
+		this.autor = a;
+	}
+	
+	public void setDescripcion(String d) {
+		this.descripcion = d;
+	}
+	
+	public void setAnio(int a) {
+		this.anioPub = a;
+	}
+	
+	public void setPag(int p) {
+		this.cantPag = p;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public String getAutor() {
+		return autor;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public int getAnio() {
+		return anioPub;
+	}
+	
+	public int getPag() {
+		return cantPag;
+	}
+	
+	// metodos propios
+	
+	public void mostrarDatos() {
+		System.out.println("Los datos de los libros son: ");
+		System.out.println("Nombre: " +this.nombre);
+		System.out.println("Autor: " +this.autor);
+		System.out.println("Descripcion: " +this.descripcion);
+		System.out.println("Año de publicación: " +this.anioPub);
+		System.out.println("Cantidad de páginas: " +this.cantPag);
+	}
 }
--- src/Principal.java
+++ src/Principal.java
@@ -6,15 +6,17 @@ public class Principal {
 	
 		Conexion conn = new Conexion();
 		Socio soc = new Socio();
+		Libro lib = new Libro();
 		
-		cargarDatos(soc);
+		cargarDatosSocio(soc);
+		cargarDatosLibro(lib);
 		conn.conectar();
 		soc.mostrarDatos();
-		
+		lib.mostrarDatos();
 
 	}
 	
-	public static void cargarDatos(Socio per) {
+	public static void cargarDatosSocio(Socio per) {
 		Scanner teclado = new Scanner(System.in);
 		
 		System.out.println("Ingrese el nombre");
@@ -32,6 +34,22 @@ public class Principal {
 		per.setMembresia(teclado.nextInt());
 		System.out.println("Está al día? 1 para Sí 2 para No");
 		per.setCuenta(teclado.nextInt());
+	}
+	
+	public static void cargarDatosLibro(Libro lib) {
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Ingrese el nombre del libro");
+		lib.setNombre(teclado.next());
+		System.out.println("Ingrese el autor del libro");
+		lib.setAutor(teclado.next());
+		System.out.println("Ingrese la descripción del libro");
+		lib.setDescripcion(teclado.next());
+		teclado.nextLine();
+		System.out.println("Ingrese el año de publicación del libro");
+		lib.setAnio(teclado.nextInt());
+		System.out.println("Ingrese la cantidad de páginas del libro");
+		lib.setPag(teclado.nextInt());		
 	}
 
 }
